df1013bec176ac077bba0c22d277f0daf80dbc2e
James Stafford <James@Jamess-Air.hitronhub.home>
2018-06-23 12:32:39 -0700

    Initial Commit

---
+++ Auto-Layout Exercise/Auto-Layout Exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto-Layout Exercise
+//
+//  Created by James Stafford on 2017-06-13.
+//  Copyright © 2017 James Stafford-Coyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
