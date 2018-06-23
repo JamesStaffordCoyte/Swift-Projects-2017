35a0e18d15190f5008f7df2c056a676b832d148c
James Stafford <James@Jamess-Air.hitronhub.home>
2018-06-23 12:47:30 -0700

    Initial commit

---
+++ FaceIt/FaceIt/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by James Stafford on 2017-06-10.
+//  Copyright © 2017 James Stafford-Coyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+}
+
